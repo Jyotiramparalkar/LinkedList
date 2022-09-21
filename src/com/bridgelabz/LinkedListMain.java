3490e1637b31ae3c6b4a02bc965c7d8b81d419b2
Jyotiramparalkar <paralkarjyotiram@gmail.com>
2022-09-21 23:37:18 +0530

    Started LinkedList Programm Computation

---
+++ src/com/bridgelabz/LinkedListMain.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+public class LinkedListMain 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to linked list problem Computation ::");
+
+        LinkedList linkedList = new LinkedList();
+        linkedList.createLinkedList();
+        linkedList.displayLinkedList();
+
+    }
+}
